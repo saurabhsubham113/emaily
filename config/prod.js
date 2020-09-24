455633725b1c366f603c364782213a6cf1e09893
subham saurabh <saurabhsubahm113@gmail.com>
2020-09-24 14:24:33 +0530

    fixed for heroku

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    GoogleClientId: process.env.GoogleClientId,
+    GoogleClientSecret: process.env.GoogleClientSecret,
+    MONGOURI: process.env.MONGOURI,
+    COOKIE_KEY: process.env.COOKIE_KEY
+}
